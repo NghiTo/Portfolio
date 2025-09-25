8085b59d90de408fea7f75bf878854970da64456
Kenzy <tobanghi@gmail.com>
2025-09-25 10:13:30 +0700

    design: navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,10 +1,31 @@
 import { assets } from "@/assets/assets";
 import Image from "next/image";
-import React from "react";
+import React, { useRef } from "react";
 
 const Navbar = () => {
+  const sideMenuRef = useRef<HTMLUListElement>(null);
+
+  const openMenu = () => {
+    if (sideMenuRef.current) {
+      sideMenuRef.current.style.transform = "translateX(-16rem)";
+    }
+  };
+
+  const closeMenu = () => {
+    if (sideMenuRef.current) {
+      sideMenuRef.current.style.transform = "translateX(16rem)";
+    }
+  };
+
   return (
     <>
+      <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
+        <Image
+          alt="header-bg-color"
+          src={assets.header_bg_color}
+          className="w-full"
+        />
+      </div>
       <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
         <a href="#top">
           <Image
@@ -15,22 +36,35 @@ const Navbar = () => {
         </a>
         <ul className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white/50 shadow-sm">
           <li>
-            <a className="font-Ovo" href="#top">Home</a>
+            <a className="font-Ovo" href="#top">
+              Home
+            </a>
           </li>
           <li>
-            <a className="font-Ovo" href="#about">About me</a>
+            <a className="font-Ovo" href="#about">
+              About me
+            </a>
           </li>
           <li>
-            <a className="font-Ovo" href="#services">Services</a>
+            <a className="font-Ovo" href="#services">
+              Services
+            </a>
           </li>
           <li>
-            <a className="font-Ovo" href="#work">My work</a>
+            <a className="font-Ovo" href="#work">
+              My work
+            </a>
           </li>
           <li>
-            <a className="font-Ovo" href="#contact">Contact me</a>
+            <a className="font-Ovo" href="#contact">
+              Contact me
+            </a>
           </li>
         </ul>
-        <div>
+        <div className="flex items-center gap-4">
+          <button>
+            <Image alt="moon-icon" className="w-6" src={assets.moon_icon} />
+          </button>
           <a
             href="#contact"
             className="hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo"
@@ -38,7 +72,54 @@ const Navbar = () => {
             Contact{" "}
             <Image alt="contact" src={assets.arrow_icon} className="w-3" />
           </a>
+          <button className="block md:hidden ml-3">
+            <Image
+              alt=""
+              src={assets.menu_black}
+              className="w-6"
+              onClick={openMenu}
+            />
+          </button>
         </div>
+
+        {/*Mobile menu*/}
+        <ul
+          ref={sideMenuRef}
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500"
+        >
+          <div className="absolute right-6 top-6" onClick={closeMenu}>
+            <Image
+              src={assets.close_black}
+              alt="close-black"
+              className="w-5 cursor-pointer"
+            />
+          </div>
+          <li>
+            <a className="font-Ovo" href="#top" onClick={closeMenu}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#about" onClick={closeMenu}>
+              About me
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#services" onClick={closeMenu}>
+              Services
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#work" onClick={closeMenu}>
+              My work
+            </a>
+          </li>
+          <li>
+            <a className="font-Ovo" href="#contact" onClick={closeMenu}>
+              Contact me
+            </a>
+          </li>
+        </ul>
       </nav>
     </>
   );
--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,7 +1,15 @@
-import logo from "@/assets/logo.png"
-import arrow_icon from "@/assets/arrow-icon.png"
+import logo from "@/assets/logo.png";
+import arrow_icon from "@/assets/arrow-icon.png";
+import header_bg_color from "@/assets/header-bg-color.png";
+import moon_icon from "@/assets/moon_icon.png";
+import menu_black from "@/assets/menu-black.png";
+import close_black from "@/assets/close-black.png";
 
 export const assets = {
-    logo,
-    arrow_icon
-}
+  logo,
+  arrow_icon,
+  header_bg_color,
+  moon_icon,
+  menu_black,
+  close_black,
+};
