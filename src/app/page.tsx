6a0dcfa3e5f57d018e524956c10d3ff53a916bff
Kenzy <tobanghi@gmail.com>
2026-04-14 00:14:35 +0700

    des: add Contact component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Services from "./components/Services";
@@ -12,7 +13,8 @@ export default function Home() {
       <Header />
       <About />
       <Services />
-      <Work/>
+      <Work />
+      <Contact />
     </>
   );
 }
